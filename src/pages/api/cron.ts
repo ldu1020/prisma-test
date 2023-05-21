7060910477c5d743a9f641efc8336c5de48c6efb
ldu1020 <ldu1020@gmail.com>
2023-05-21 23:59:05 +0900

    fix: 🐛move vercel.json, for cron

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -3,8 +3,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 import prisma from '../../../lib/prisma';
 
-export default async function email(req: NextApiRequest, res: NextApiResponse) {
-  const created = await prisma.post.create({ data: { title: 'test' } });
+export default async function cron(req: NextApiRequest, res: NextApiResponse) {
+  const created = await prisma.post.create({ data: { title: 'cron' } });
 
   res.status(200).json(created);
 }
